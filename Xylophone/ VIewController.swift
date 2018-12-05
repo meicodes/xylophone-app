3c26e83f5d965add323191172bc81973aef12fd4
Amei Mbah <nottlynn@gmail.com>
2018-12-04 22:04:25 -0500

    finished module

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -9,11 +9,9 @@
 import UIKit
 import AVFoundation
 
-
-class ViewController: UIViewController{
-    
-    
-    var player : AVAudioPlayer?
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    var player : AVAudioPlayer!
+    let soundArray = ["note1","note2","note3","note4","note5","note6","note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,19 +20,19 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+        playSound(file: soundArray[sender.tag - 1])
+    }
+    
+    func playSound(file : String) {
+        let url = Bundle.main.url(forResource: file, withExtension: "wav")
         
         do {
-            player = try AVAudioPlayer(contentsOf: url)
-            guard let player = player else { return }
-            
-            player.prepareToPlay()
-            player.play()
+            player = try AVAudioPlayer(contentsOf: url!)
         } catch let error as Error {
             print(error.localizedDescription)
         }
         
+        player.play()
     }
     
   
